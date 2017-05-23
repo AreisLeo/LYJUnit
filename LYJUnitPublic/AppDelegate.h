d96740253eff4a9f2d3c75739c14151d34668091
Aries_Leo <361467298@qq.com>
2017-05-23 21:30:56 +0800

    初始数据提交

---
+++ LYJUnitPublic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYJUnitPublic
+//
+//  Created by Aries li on 2017/5/23.
+//  Copyright © 2017年 Aries li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYJUnitPublic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYJUnitPublic
+//
+//  Created by Aries li on 2017/5/23.
+//  Copyright © 2017年 Aries li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
